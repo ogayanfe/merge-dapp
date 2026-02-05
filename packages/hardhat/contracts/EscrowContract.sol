f40abba7cff08399b8432e4ce7acfc5067fdd374
Odule Ayanfeoluwa Grandeur <95624629+ogayanfe@users.noreply.github.com>
2026-02-06 00:25:38 +0100

    feat: updated smart contract

---
+++ packages/hardhat/contracts/EscrowContract.sol
@@ -0,0 +1,131 @@
+//SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0 <0.9.0;
+
+enum VerificationMode {
+    RepoValidation,
+    ManualValidation
+}
+
+enum EscrowState {
+    OPEN,
+    APPLIED,
+    LOCKED,
+    IN_REVIEW,
+    DISPUTED,
+    COMPLETED,
+    CANCELLED
+}
+
+contract EscrowContract {
+    // ERRORS
+    error NotAuthorised();
+    error InvalidForContractState();
+    error InvalidContractEthValue();
+    error PaymentFailed();
+
+    // EVENTS
+    event ApplicationRecieved(address client, address applicant, uint256 timestamp);
+    event JobSubmitted(address freelancer, uint256 timestamp, VerificationMode mode);
+    event DisputeRaised(address client, uint256 timestamp, VerificationMode mode);
+
+    // STATE VARIABLES
+    address public client;
+    address public freelancer;
+    address public arbiter;
+    string public IPFSHash;
+    string public repoURL;
+    uint256 public deployTime;
+    address[] public applicants;
+    EscrowState public state;
+    uint256 autoReleaseDeadline;
+    VerificationMode public verificationMode;
+
+    // CONSTANTS
+    uint256 public constant reviewWindow = 1 weeks;
+    uint256 public constant disputePercent = 0.01 ether; // 1% of 1 ETH
+
+    modifier onlyClient() {
+        if (msg.sender != client) revert NotAuthorised();
+        _;
+    }
+
+    modifier onlyFreelancer() {
+        if (msg.sender != freelancer) revert NotAuthorised();
+        _;
+    }
+
+    modifier onlyArbiter() {
+        if (msg.sender != freelancer) revert NotAuthorised();
+        _;
+    }
+
+    modifier inState(EscrowState _state) {
+        if (_state != state) revert InvalidForContractState();
+        _;
+    }
+
+    constructor(address _client, address _arbiter, string memory _IPFSHash, string memory _repoURL, VerificationMode _mode) payable {
+        if (msg.value == 0) revert InvalidContractEthValue();
+
+        client = _client;
+        arbiter = _arbiter;
+        IPFSHash = _IPFSHash;
+        repoURL = _repoURL;
+        verificationMode = _mode;
+    }
+
+    function applyAsFreelancer() external {
+        if (state != EscrowState.OPEN && state != EscrowState.APPLIED) revert InvalidForContractState();
+        applicants.push(msg.sender);
+        state = EscrowState.APPLIED;
+    }
+
+    function hireFreelancer(address _dev) external onlyClient inState(EscrowState.APPLIED) {
+        freelancer = _dev;
+        state = EscrowState.LOCKED;
+    }
+
+    function submitWork() external onlyFreelancer inState(EscrowState.LOCKED) {
+        state = EscrowState.IN_REVIEW;
+        autoReleaseDeadline = block.timestamp + reviewWindow;
+
+        emit JobSubmitted(msg.sender, block.timestamp, verificationMode);
+    }
+
+    function completeJob() external onlyClient inState(EscrowState.IN_REVIEW) {
+        state = EscrowState.COMPLETED;
+        payFreelancer();
+    }
+
+    function dispute() external onlyClient inState(EscrowState.IN_REVIEW) {
+        state = EscrowState.DISPUTED;
+        emit DisputeRaised(msg.sender, block.timestamp, verificationMode);
+    }
+
+    function autoRelease() external inState(EscrowState.IN_REVIEW) {
+        if (block.timestamp < autoReleaseDeadline) revert InvalidForContractState();
+        state = EscrowState.COMPLETED;
+        payFreelancer();
+    }
+
+    function payFreelancer() public inState(EscrowState.COMPLETED) {
+        (bool success, ) = payable(freelancer).call{ value: address(this).balance }("");
+        if (!success) revert PaymentFailed();
+    }
+
+    function refundClient() external inState(EscrowState.CANCELLED) {
+        (bool success, ) = payable(client).call{ value: address(this).balance }("");
+        if (!success) revert PaymentFailed();
+    }
+
+    function chargeDisputeFeed() private inState(EscrowState.COMPLETED) {
+        uint256 arbiterFee = (address(this).balance * disputePercent) / 1 ether;
+        (bool success, ) = payable(arbiter).call{ value: arbiterFee }("");
+        if (!success) revert PaymentFailed();
+    }
+
+    function resolveDispute(bool _payFreelancer) external onlyArbiter {
+        state = _payFreelancer ? EscrowState.COMPLETED : EscrowState.CANCELLED;
+        chargeDisputeFeed();
+    }
+}
---
+++ packages/hardhat/contracts/MergeFactory.sol
@@ -0,0 +1,44 @@
+//SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0 <0.9.0;
+
+import "hardhat/console.sol";
+
+// Use openzeppelin to inherit battle-tested implementations (ERC20, ERC721, etc)
+import "@openzeppelin/contracts/access/Ownable.sol";
+import { EscrowContract, VerificationMode } from "./EscrowContract.sol";
+
+struct JobMetadata {
+    uint256 index;
+    address client;
+    address escrowAddress;
+    string IPFSHash;
+}
+
+contract MergeFactory is Ownable {
+    // ERROR 
+    error InvalidAmount(); 
+
+    
+    // STATE VARIABLES
+    address arbiter;
+    JobMetadata[] public jobs;
+    mapping(address => JobMetadata[]) public userJobs;
+
+    constructor(address _owner, address _arbiter) Ownable(_owner) {
+        arbiter = _arbiter;
+    }
+
+    function postJob(string memory _IPFSHash, string memory _repoURL, VerificationMode _verificationMode) external payable {
+        if (msg.value == 0) revert InvalidAmount(); 
+        EscrowContract newEscrow = new EscrowContract{value: msg.value}(msg.sender, arbiter, _IPFSHash, _repoURL, _verificationMode);
+
+        JobMetadata memory newJob = JobMetadata({ index: jobs.length, client: msg.sender, escrowAddress: address(newEscrow), IPFSHash: _IPFSHash });
+
+        jobs.push(newJob);
+        userJobs[msg.sender].push(newJob);
+    }
+
+    function getJobsByClient(address _client) external view returns (JobMetadata[] memory) {
+        return userJobs[_client];
+    }
+}
